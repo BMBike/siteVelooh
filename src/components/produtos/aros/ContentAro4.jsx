849feef7036761b535ca62056e639721b4a78a79
ThiKoiama <thiagokoiama@gmail.com>
2022-05-23 09:48:28 -0300

    commit

---
+++ src/components/produtos/aros/ContentAro4.jsx
@@ -0,0 +1,90 @@
+import styles from "../../../styles/produtos/ContentProdutos.module.css";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
+import { useState } from "react";
+
+export default function ContentAro4() {
+  const [cor, setCor] = useState("Laranja");
+  const[tam, setTam] = useState("20, 24, 26 e 26 x 1.1/2");
+  const [foto, setFoto] = useState(
+    "/images/produtos/aros/neon/laranja.png"
+  );
+
+  function mudarCor(cor, foto, tam) {
+    setCor(cor);
+    setFoto(foto);
+    setTam(tam)
+  }
+  return (
+    <div className={styles.prods}>
+      <h1 style={{ fontSize: "4rem", marginTop: "60px", marginLeft: "60px" }}>
+        AROS:
+      </h1>
+      <div className={styles.produtos}>
+        <div className={styles.produto}>
+          <div className={styles.Moldfoto}>
+            <Zoom>
+              <img src={foto} alt="Produto" className={styles.imgProd} />
+            </Zoom>
+          </div>
+          <div className={styles.lateral}>
+            <div className={styles.texto}>
+              <p>
+                ARO AERO NEON {cor} VELOOH
+              </p>
+            </div>
+            <div className={styles.cores}>
+            <div
+                style={{marginLeft:"0px"}}
+                className={styles.laranja}
+                onClick={() =>
+                  mudarCor(
+                    "Laranja",
+                    "/images/produtos/aros/neon/laranja.png",
+                    "20, 24, 26 e 26 x 1.1/2"
+                  )
+                }
+              ></div>
+            <div
+                className={styles.rosa}
+                onClick={() =>
+                  mudarCor(
+                    "Rosa",
+                    "/images/produtos/aros/neon/rosa.png",
+                    "20, 24, 26 e 26 x 1.1/2"
+                  )
+                }
+              ></div>
+            
+            <div
+                className={styles.verdeclaro}
+                onClick={() =>
+                  mudarCor(
+                    "Verde",
+                    "/images/produtos/aros/neon/verde.png",
+                    "20, 24 e 26"
+                  )
+                }
+              ></div>
+            <div
+                className={styles.amarelo}
+                onClick={() =>
+                  mudarCor(
+                    "Amarelo",
+                    "/images/produtos/aros/neon/amarelo.png",
+                    "26 x 1.1/2"
+                  )
+                }
+              ></div>  
+            </div>
+            <div className={styles.texto2}>
+            <p> TAMANHOS: {tam}</p>
+            </div>
+  
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/produtos/aros/aeroneon.jsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Header from "../../../components/Header";
+import Footer from "../../../components/Footer";
+import ContentAro4 from "../../../components/produtos/aros/ContentAro4";
+import Zap2 from "../../../components/Zap2";
+
+export default function aeroneon() {
+  return (
+    <div style={{backgroundColor: "#2d2d2d"}}>
+      <Head>
+        <title>Velooh</title>
+        <meta name="description" content="Velooh" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/images/icon.ico" type="image/x-icon" />
+      </Head>
+      <Header />
+      <ContentAro4 />
+      <Footer />
+      <Zap2 />
+    </div>
+  );
+}
