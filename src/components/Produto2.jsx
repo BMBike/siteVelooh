4f8c4603cc3ade7e12243b71baa9ae9383fd92bd
ThiKoiama <thiagokoiama@gmail.com>
2022-05-23 14:09:15 -0300

    commit

---
+++ src/components/Produto2.jsx
@@ -0,0 +1,28 @@
+
+import styles from "../styles/Produto.module.css"
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
+export default function Produto(props) {
+  return (
+    <div>
+      <center><h3 style={{fontSize:"2rem"}}>{props.titulo}</h3></center>
+     <div className={styles.prodMold}>
+       <Zoom>
+       <img src={props.img} alt="Produto" className={styles.imgProd} />
+       </Zoom>
+            
+            <div className={styles.cores}>
+              <div className={styles[props.cor1]}></div>
+              <div className={styles[props.cor2]}></div>
+              <div className={styles[props.cor3]}></div>
+              <div className={styles[props.cor4]}></div>
+              <div className={styles[props.cor5]}></div>
+              <div className={styles[props.cor6]}></div>
+              <div className={styles[props.cor7]}></div>
+              <div className={styles[props.cor8]}></div>
+              <div className={styles[props.cor9]}></div>
+            </div>
+          </div>
+    </div>
+  );
+}
--- src/pages/produtos/gancheiras/index.jsx
+++ src/pages/produtos/gancheiras/index.jsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Header from "../../../components/Header";
 import Footer from "../../../components/Footer";
-import Zap2 from "../../../components/Zap2";
+import Zap from "../../../components/Zap";
 import ProdutosPg from "../../../components/ProdutosPg";
-import Produto from "../../../components/Produto";
+import Produto2 from "../../../components/Produto2";
+
+
 
 export default function indexGancheiras() {
   return (
@@ -16,11 +18,114 @@ export default function indexGancheiras() {
       </Head>
       <Header />
       <ProdutosPg title="GANCHEIRAS">
-        
+       <Produto2 titulo="EXPOSITOR GANCHEIRA" img="/images/produtos/gancheiras/expositor.jpg"/> 
+       <Produto2 titulo="ALFAMEC/CILT/TIRRENO ATX/ZT3 N°94" img="/images/produtos/gancheiras/1672.png"/> 
+       <Produto2 titulo="ALFAMEC/WNY KAIROS XKS N°01" img="/images/produtos/gancheiras/0132.jpeg"/> 
+       <Produto2 titulo="ALFAMEQ STROLL N°09" img="/images/produtos/gancheiras/0133.jpg"/> 
+       <Produto2 titulo="ALFAMEQ/CANADIAN N°15" img="/images/produtos/gancheiras/0134.jpg"/> 
+       <Produto2 titulo="ALFAMEQ/CANADIAN X-TERRA N°17" img="/images/produtos/gancheiras/0135.jpg"/> 
+       <Produto2 titulo="ASTRO/GROOVE N°85" img="/images/produtos/gancheiras/0663.jpg"/> 
+       <Produto2 titulo="ASTRO DEFENSE/GTS R3/CALOI SPRINT N°48" img="/images/produtos/gancheiras/0136.jpg"/> 
+       <Produto2 titulo="AUDAX AUGE 555/600/ 700 N°92" img="/images/produtos/gancheiras/1677.png"/> 
+       <Produto2 titulo="AUDAX100/101/200/300 2021 AUGE 600/700 2018 N°89" img="/images/produtos/gancheiras/1674.png"/> 
+       <Produto2 titulo="BH DOWNHILL/AUDAX AUGE 30/40 N°38" img="/images/produtos/gancheiras/0137.jpg"/> 
+       <Produto2 titulo="BMC TIME MACHINE N°82" img="/images/produtos/gancheiras/0660.jpg"/> 
+       <Produto2 titulo="CALOI EL 10/20/KODE CORRATEC SUPERBOW N°12" img="/images/produtos/gancheiras/0138.jpg"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/> 
+       <Produto2 titulo="" img="/images/produtos/gancheiras/"/>
+      
+
+       
       </ProdutosPg>
 
       <Footer />
-      <Zap2 />
+      <Zap />
     </div>
   );
 }
