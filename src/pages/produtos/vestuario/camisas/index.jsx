effa0d74a6b2bd01348e402ce333156dabb96f31
ThiKoiama <thiagokoiama@gmail.com>
2022-05-30 13:41:42 -0300

    COMMIT

--- src/pages/produtos/vestuario/camisas/index.jsx
+++ src/pages/produtos/vestuario/camisas/index.jsx
@@ -15,16 +15,16 @@ export default function camisas() {
       </Head>
       <Header />
       <ProdutosPg title="camisas">
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa de Corrida velooh" img="/images/produtos/vestuario/camisa/corrida.png"/>
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa Fem. Infantil Quasar"  img="/images/produtos/vestuario/camisa/feminfantquasarazul.png"/>
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa Fem. Classic Jet"  img="/images/produtos/vestuario/camisa/femclassicjetazul.jpg"/>
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa Fem. Gold"  img="/images/produtos/vestuario/camisa/femgoldazul.jpg"/>
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa Fem. Quasar"  img="/images/produtos/vestuario/camisa/femquasarazulrosa.png"/>
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa Masc. Infantil Wind"  img="/images/produtos/vestuario/camisa/mascinfantilwind.png"/>
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa Masc. Infantil Wind C/manga longa"  img="/images/produtos/vestuario/camisa/mascinfantilwindmangaazul.png"/>
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa Masc. Classic Jet"  img="/images/produtos/vestuario/camisa/masclassicjetazul.jpg"/>
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa Masculina Gold" img="/images/produtos/vestuario/camisa/mascgoldazul.jpeg"/>
-      <Produto3 href="/produtos/vestuario/camisas" titulo="Camisa Masc. Wind" img="/images/produtos/vestuario/camisa/mascazulvd.png"/>
+      <Produto3 href="/produtos/vestuario/camisas/corrida" titulo="Camisa de Corrida velooh" img="/images/produtos/vestuario/camisa/corrida.png"/>
+      <Produto3 href="/produtos/vestuario/camisas/feminfquasar" titulo="Camisa Fem. Infantil Quasar" cor1="azulClaro" cor2="azulrosa" cor3="rosrox" img="/images/produtos/vestuario/camisa/feminfantquasarazul.png"/>
+      <Produto3 href="/produtos/vestuario/camisas/femclassicjet" titulo="Camisa Fem. Classic Jet"  img="/images/produtos/vestuario/camisa/femclassicjetazul.jpg"/>
+      <Produto3 href="/produtos/vestuario/camisas/femgold" titulo="Camisa Fem. Gold"  img="/images/produtos/vestuario/camisa/femgoldazul.jpg"/>
+      <Produto3 href="/produtos/vestuario/camisas/femquasar" titulo="Camisa Fem. Quasar"  img="/images/produtos/vestuario/camisa/femquasarazulrosa.png"/>
+      <Produto3 href="/produtos/vestuario/camisas/mascinfwind" titulo="Camisa Masc. Infantil Wind"  img="/images/produtos/vestuario/camisa/mascinfantilwind.png"/>
+      <Produto3 href="/produtos/vestuario/camisas/mascinfwindmanga" titulo="Camisa Masc. Infantil Wind C/manga longa"  img="/images/produtos/vestuario/camisa/mascinfantilwindmangaazul.png"/>
+      <Produto3 href="/produtos/vestuario/camisas/mascclassicjet" titulo="Camisa Masc. Classic Jet"  img="/images/produtos/vestuario/camisa/masclassicjetazul.jpg"/>
+      <Produto3 href="/produtos/vestuario/camisas/mascgold" titulo="Camisa Masculina Gold" img="/images/produtos/vestuario/camisa/mascgoldazul.jpeg"/>
+      <Produto3 href="/produtos/vestuario/camisas/mascwind" titulo="Camisa Masc. Wind" img="/images/produtos/vestuario/camisa/mascazulvd.png"/>
       </ProdutosPg>
       <Footer />
       <Zap2 />
