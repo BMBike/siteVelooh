6fe614e004081779c9b397409b0061aaf6c5990c
ThiKoiama <thiagokoiama@gmail.com>
2022-06-01 14:20:19 -0300

    commit

--- src/components/produtos/vestuario/camisas/ContentCamisa8.jsx
+++ src/components/produtos/vestuario/camisas/ContentCamisa8.jsx
@@ -4,10 +4,10 @@ import "react-medium-image-zoom/dist/styles.css";
 import { useState } from "react";
 
 export default function ContentCamisa8() {
-    const [cor, setCor] = useState("Azul");
-    const[tam, setTam] = useState("P, M, G E GG");
+    const [cor, setCor] = useState("LISTRA AZUL/VERDE");
+    const[tam, setTam] = useState("2, 4, 6, 8 e 10");
     const [foto, setFoto] = useState(
-      "/images/produtos/vestuario/manguito/ptazul.png"
+      "/images/produtos/vestuario/camisa/mascinfantilwind.png"
     );
     function mudarCor(cor, foto, tam) {
       setCor(cor);
@@ -17,7 +17,7 @@ export default function ContentCamisa8() {
     return (
       <div className={styles.prods}>
         <h1 style={{ fontSize: "4rem", marginTop: "60px", marginLeft: "60px" }}>
-          Manguitos:
+          Camisas:
         </h1>
         <div className={styles.produtos}>
           <div className={styles.produto}>
@@ -29,37 +29,62 @@ export default function ContentCamisa8() {
             <div className={styles.lateral}>
               <div className={styles.texto}>
                 <p>
-                MANGUITO C/ PROTEÇÃO UV PRETO DETALHE {cor}
+                CAMISA MASC INFANTIL WIND {cor}
                 </p>
               </div>
               <div className={styles.cores}>
-                <div
-                  className={styles.azul}
+              <div
+                  className={styles.azulverdepiscina}
+                  style={{marginLeft:'0'}}
                   onClick={() =>
                     mudarCor(
-                      "Azul",
-                      "/images/produtos/vestuario/manguito/ptazul.png",
-                      "P, M, G E GG"
+                      "LISTRA AZUL/VERDE",
+                      "/images/produtos/vestuario/camisa/mascinfantilwind.png",
+                      "2, 4, 6, 8 e 10"
+                    )
+                  }
+                ></div>
+                
+                
+                <div
+                  className={styles.ptverdepiscina}
+                  style={{marginLeft:'15px'}}
+                  onClick={() =>
+                    mudarCor(
+                      "AZUL/VERDE AGUA",
+                      "/images/produtos/vestuario/camisa/mascinfwindverocean.png",
+                      "2, 4, 6 e 8"
+                    )
+                  }
+                ></div>
+               
+                <div
+                  className={styles.azulvermelho}
+                  onClick={() =>
+                    mudarCor(
+                      "DEGRADE VERMELHO/AZUL",
+                      "/images/produtos/vestuario/camisa/feminfantquasarcinzarosa.png",
+                      "2,6 e 8"
                     )
                   }
                 ></div>
                 <div
-                  className={styles.rosa}
+                  className={styles.ptamarelo}
                   onClick={() =>
                     mudarCor(
-                      "Rosa",
-                      "/images/produtos/vestuario/manguito/ptrosa.png",
-                      "P, M E G"
+                      "PRETO/AMARELO",
+                      "/images/produtos/vestuario/camisa/mascinfwindptamarelo.png",
+                      "2, 4, 6, 8, 10 e 12"
                     )
                   }
                 ></div>
                 <div
-                  className={styles.polido}
+                  className={styles.vermelho}
                   onClick={() =>
                     mudarCor(
-                      "Refletivo",
-                      "/images/produtos/vestuario/manguito/ptrefletivo.png",
-                      "P, M, G E GG"
+                      "VERMELHO",
+                      "/images/produtos/vestuario/camisa/mascwindinfvermelho.png",
+                      "2, 4, 8 e 10"
                     )
                   }
                 ></div>
