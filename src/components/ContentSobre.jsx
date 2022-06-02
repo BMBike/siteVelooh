79bdf2390f84588403070415e9cca59e74d16753
ThiKoiama <thiagokoiama@gmail.com>
2022-06-02 12:30:24 -0300

    commit

--- src/components/ContentSobre.jsx
+++ src/components/ContentSobre.jsx
@@ -9,7 +9,7 @@ export default function ContentSobre() {
         </div>
 
         <div className={styles.hist}>
-          <h2>Nossa História</h2>
+          <h1>Nossa História</h1>
           <br />
           <p>
             O(A) BM Bike foi fundado(a) em 2000 com uma missão: trazer um(a)
@@ -26,7 +26,7 @@ export default function ContentSobre() {
           <h1>Origem da loja</h1>
         </div>
         <div className={styles.servico}>
-          <h1>SERVIÇO PESSOAL</h1>
+          <h1>Serviço pessoal</h1>
           <p>
             Serviço pessoal No(a) BM Bike somos motivados pela nossa dedicação
             em oferecer um serviço superior e personalizado. Não podemos deixar
