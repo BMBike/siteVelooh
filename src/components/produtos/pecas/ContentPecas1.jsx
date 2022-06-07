2ee901bb89dad5be2deb4e484f09204cb91d8470
ThiKoiama <thiagokoiama@gmail.com>
2022-06-07 11:05:51 -0300

    COMMIT

---
+++ src/components/produtos/pecas/ContentPecas1.jsx
@@ -0,0 +1,9 @@
+import styles from "../../../styles/produtos/ContentPecas.module.css"
+
+export default function ContentPecas1() {
+  return (
+    <div>
+
+    </div>
+  );
+}
--- src/pages/produtos/acessorios/index.jsx
+++ src/pages/produtos/acessorios/index.jsx
@@ -16,7 +16,23 @@ export default function indexAcessorios() {
       </Head>
       <Header />
       <ProdutosPg title="ACESSÓRIOS">
-        
+      <Produto href="/#" titulo="BOLSA DE QUADRO P/CELULAR" img="/images/produtos/" />
+      <Produto href="/#" titulo="BOLSA DE QUADRO" img="/images/produtos/" />
+      <Produto href="/#" titulo="BOLSA DE SELIM AERO" img="/images/produtos/" />
+      <Produto href="/#" titulo="BOLSA DE SELIM MTB" img="/images/produtos/" />
+      <Produto href="/#" titulo="CARAMANHOLA 300ML BABY" img="/images/produtos/" />
+      <Produto href="/#" titulo="CARAMANHOLA 500ML PRO" img="/images/produtos/" />
+      <Produto href="/#" titulo="CARAMANHOLA ISOTERMICA 600ML" img="/images/produtos/" />
+      <Produto href="/#" titulo="CARAMANHOLA TERMICA 700ML" img="/images/produtos/" />
+      <Produto href="/#" titulo="CHAVE CANIVETE ALLEN 11 FUNÇÕES" img="/images/produtos/" />
+      <Produto href="/#" titulo="CHAVE CANIVETE ALLEN 7 FUNÇÕES" img="/images/produtos/" />
+      <Produto href="/#" titulo="CICLO COMPUTADOR COM FIO" img="/images/produtos/" />
+      <Produto href="/#" titulo="KIT EXPOSITOR CARAMANHOLA" img="/images/produtos/" />
+      <Produto href="/#" titulo="PORTA OBJETOS" img="/images/produtos/" />
+      <Produto href="/#" titulo="SUPORTE CARAMANHOLA" img="/images/produtos/" />
+      <Produto href="/#" titulo="SUPORTE P/ CELULAR DE SILICONE" img="/images/produtos/" />
+      <Produto href="/#" titulo="TRANSBIKE" img="/images/produtos/" />
+
       </ProdutosPg>
 
       <Footer />
