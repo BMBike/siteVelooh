b3e6fd510578cd12f2f45b7fd3546be024428086
ThiKoiama <thiagokoiama@gmail.com>
2022-07-15 15:18:26 -0300

    commit

--- src/components/Produto2.jsx
+++ src/components/Produto2.jsx
@@ -1,7 +1,7 @@
 import styles from "../styles/Produto2.module.css";
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
-export default function Produto(props) {
+export default function Produto2(props) {
   return (
     <div
       style={{
